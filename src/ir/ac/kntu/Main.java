fdbf918b6d717cdeada38f8ad6e491fde8504fa2
shayan <malekishayan79@gmail.com>
2020-09-15 14:42:40 +0430

    cleaning main

--- src/ir/ac/kntu/Main.java
+++ src/ir/ac/kntu/Main.java
@@ -2,15 +2,24 @@ package ir.ac.kntu;
 
 public class Main {
     public static void main(String[] args) {
-        initialize();
         start();
     }
 
-    public static void initialize() {
-
+    public static void initialize(Company company) {
+        MyFile myFile = new FileHelper(company);
+        myFile.read();
     }
 
     public static void start() {
+        Company company = new Company();
+        initialize(company);
+        // goes to the menu
+        Menu.start(company);
+        finish(company);
+    }
 
+    public static void finish(Company company) {
+        MyFile file = new FileHelper(company);
+        file.write();
     }
 }
