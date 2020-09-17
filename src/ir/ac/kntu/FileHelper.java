fce8496ab612d6c3122748285caee4040b2dff9a
shayan <malekishayan79@gmail.com>
2020-09-17 16:19:22 +0430

    fixing EOF exception handling

--- src/ir/ac/kntu/FileHelper.java
+++ src/ir/ac/kntu/FileHelper.java
@@ -13,10 +13,8 @@ public class FileHelper implements MyFile {
     }
 
     public void writeProducts() {
-        try (
-                FileOutputStream file = new FileOutputStream("products");
-                ObjectOutputStream obj = new ObjectOutputStream(file)
-        ) {
+        try (FileOutputStream file = new FileOutputStream("products");
+             ObjectOutputStream obj = new ObjectOutputStream(file)) {
             for (Product product : company.getProducts()) {
                 obj.writeObject(product);
             }
@@ -26,28 +24,25 @@ public class FileHelper implements MyFile {
     }
 
     public void readProducts() {
-        try (
-                FileInputStream file = new FileInputStream("products");
-                ObjectInputStream obj = new ObjectInputStream(file)) {
+        try (FileInputStream file = new FileInputStream("products");
+             ObjectInputStream obj = new ObjectInputStream(file)) {
             while (true) {
                 try {
                     company.addProduct((Product) obj.readObject());
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
-                } catch (EOFException e) {
-                    break;
                 }
             }
+        } catch (EOFException ignored) {
         } catch (IOException e) {
+            System.out.println("here");
             e.printStackTrace();
         }
     }
 
     public void writeCustomers() {
-        try (
-                FileOutputStream file = new FileOutputStream("customers");
-                ObjectOutputStream obj = new ObjectOutputStream(file)
-        ) {
+        try (FileOutputStream file = new FileOutputStream("customers");
+             ObjectOutputStream obj = new ObjectOutputStream(file)) {
             for (Customer customer : company.getCustomers()) {
                 obj.writeObject(customer);
             }
@@ -57,20 +52,18 @@ public class FileHelper implements MyFile {
     }
 
     public void readCustomers() {
-        try (
-                FileInputStream file = new FileInputStream("customers");
-                ObjectInputStream obj = new ObjectInputStream(file)) {
+        try (FileInputStream file = new FileInputStream("customers");
+             ObjectInputStream obj = new ObjectInputStream(file)) {
             while (true) {
                 try {
-
-                    company.addCustomer(((Customer) obj.readObject()));
+                    company.addCustomer((Customer) obj.readObject());
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
-                } catch (EOFException e) {
-                    break;
                 }
             }
-        } catch (IOException e) {
+        } catch (EOFException ignored) {
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
     }
